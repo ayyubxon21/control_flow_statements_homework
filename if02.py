151189355bcb15a0bc769f146acd9445e54c0bd5
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2022-10-18 12:19:48 -0700

    fjdk

--- if02.py
+++ if02.py
@@ -6,9 +6,9 @@ def main(a):
     Returns:
         a: a increased by 1 if positive, else decreased by 2.
     """
-    if a>0:
+    if a>=0:
         return a+1
-    if a<0:
+    if a<=0:
         return a-2
-print(main(-5))
+print(main(0))
     
