474e1c21dac1f4b3de7b3c1a25dd25cf13ca2e30
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2022-10-18 12:06:41 -0700

    dkk

--- if01.py
+++ if01.py
@@ -9,7 +9,7 @@ def main(a):
     Returns:
         a: a increased by 1 if positive, else unchanged.
     """ 
-    if a>0:
+    if 1>0:
         return a+1
-    return 
+    return True
 print(main(1))
