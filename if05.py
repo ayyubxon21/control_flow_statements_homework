0d0a80e0b029c8d7edcca588de3d5af86c8a4481
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2022-10-18 22:16:37 -0700

    k

--- if05.py
+++ if05.py
@@ -18,8 +18,7 @@ def main(a,b,c):
         return 2
     if a>0 and b>0 and c<0:
         return 1
-    if a>0 and b>0 and c<0:
-        return 0 
-
-
+    if a>0 and b>0 and c>0:
+        return 0
+    
 print(main(2,4,-1))
