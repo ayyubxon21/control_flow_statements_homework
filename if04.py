72e44b352c6861ed1b7ff9a76c1b691d4925cab4
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2022-10-18 22:19:57 -0700

    d

--- if04.py
+++ if04.py
@@ -8,4 +8,17 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    if a<0 and b<0 and c<0:
+        return 0
+    if a>0 and b<0 and c<0:
+        return 1
+    if a<0 and b>0 and c<0:
+        return 1
+    if a<0 and b<0 and c>0:
+        return 1
+    if a>0 and b>0 and c<0:
+        return 2
+    if a>0 and b>0 and c>0:
+        return 3
+print(main(5,6,7))
+    
